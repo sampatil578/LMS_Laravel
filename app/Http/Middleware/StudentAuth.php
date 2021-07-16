3e751f55897432260cf53c654f5ea44a44d7f045
Samruddha <sampatil578@gmail.com>
2021-07-17 00:11:31 +0530

    page authentication added

--- app/Http/Middleware/StudentAuth.php
+++ app/Http/Middleware/StudentAuth.php
@@ -17,19 +17,37 @@ class StudentAuth
     public function handle(Request $request, Closure $next)
     {
         if($request->path()=="student_login" && ($request->session()->has('student')||$request->session()->has('admin'))){
-            return redirect("/");
+            return redirect("/books");
         }
-        if($request->path()=="student_signup" && $request->session()->has('student')){
-            return redirect("/");
+        if($request->path()=="student_signup" && ($request->session()->has('student')||$request->session()->has('admin'))){
+            return redirect("/books");
         }
         if($request->path()=="admin_login" && ($request->session()->has('student')||$request->session()->has('admin'))){
-            return redirect("/");
+            return redirect("/books");
         }
         if($request->path()=="addadmin" && !$request->session()->has('admin')){
-            return redirect("/");
+            return redirect("/books");
+        }
+        if($request->path()=="addbooks" && !$request->session()->has('admin')){
+            return redirect("/books");
+        }
+        if($request->path()=="bbooks" && !$request->session()->has('admin')){
+            return redirect("/books");
+        }
+        if($request->path()=="bookrequests" && !$request->session()->has('admin')){
+            return redirect("/books");
+        }
+        if($request->path()=="fail" && !$request->session()->has('student')){
+            return redirect("/books");
+        }
+        if($request->path()=="mybooks" && !$request->session()->has('student')){
+            return redirect("/books");
+        }
+        if($request->path()=="profle" && !$request->session()->has('student')){
+            return redirect("/books");
         }
         if($request->path()=="students" && !$request->session()->has('admin')){
-            return redirect("/");
+            return redirect("/books");
         }
         return $next($request);
     }
